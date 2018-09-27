308adf81286f100462b86c5d41df4553dffb028b
Marc Teunis <marc.teunis@hu.nl>
2018-09-27 12:24:19 +0200

    materials version 1

---
+++ demos/1_rstudio_base_demo.R
@@ -0,0 +1,62 @@
+
+
+## Workshop programme
+
+10:30 start
+
+10:40 Case ("Big Data in Psychiatry") - 'Karin Hagoort'
+
+11:00 Hands-on Part 1; 
+
+DATASET for the workshop: Pertussis outbreaks in Europe from 1990 - 2017 
+
+ - Github - clonen/commit/push/pull (10 min)
+ - RSTUDIO / INTERFACE demo R-base: (30 min)
+  * objects
+  * global environment
+  * writing scripts 
+  * class of objects
+  * dataframes
+  * importing data via "import data" in RStudio
+  * class of tibble? also a dataframe
+  * panes
+  * View
+  * Opening files, running scripts
+  * Import data
+  * Documentation / help functie `?` vs `??`
+  * BrowseVignettes / Vignettes
+  * code completion
+  * appearance 
+
+11:45
+
+ --LUNCH-- 
+
+12:30
+
+ * recap + base exercise (25 min)
+
+ - RMarkdown running code and code chuncks
+ - Plotting ggplot2
+ - Exercise ggplot2 
+
+14:30
+
+ -- BREAK --
+
+15:00 
+Case (o.a. Shiny
+
+ - dplyr demo
+ - Exercise?
+
+
+ 
+
+
+
+
+
+
+
+
